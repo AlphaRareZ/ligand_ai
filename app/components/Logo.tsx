4d31dc194a747c8f08c8c6bf0a345ada8d883187
AlphaRareZ <dodo2003pro@gmail.com>
2026-03-23 02:40:56 +0200

    solved issues in header

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -6,9 +6,9 @@ export default function Logo({ className = "" }) {
   return (
     <div className={`${className}`}>
       <Link href="/">
-        <h1 className="text-2xl font-bold flex items-center gap-4 shrink-0">
+        <h1 className="text-xl md:text-2xl font-bold flex items-center gap-4 shrink-0">
           <div className="p-4 bg-[#0f1b37] rounded-xl">
-          <FontAwesomeIcon icon={faDna} className="h-6 w-6 text-[#1152d4]" />
+          <FontAwesomeIcon icon={faDna} className="w-4 h-4 md:h-6 md:w-6 text-[#1152d4]" />
 
           </div>
           Ligand AI
