f9baeb0ef1e2c1850f9a9594c210135df09cfa56
AlphaRareZ <dodo2003pro@gmail.com>
2026-05-19 19:44:43 +0300

    asd

--- app/(site)/about-us/components/TeamCarousel.tsx
+++ app/(site)/about-us/components/TeamCarousel.tsx
@@ -41,7 +41,7 @@ const TEAM: TeamMember[] = [
         image: "https://pub-a36c7e0b4a1444c48def1977e87f7a9c.r2.dev/uploads/Muhammed.jpg",
         linkedin: "https://www.linkedin.com/",
         email: "mailto:Muhammedalaa.404@gmail.com",
-        tags: ["DevOps", "Software Architecture", "MLOps"],
+        tags: [ "Software Architecture","DevOps", "Backend Engineer"],
     },
     {
         name: "Mariam Muhammed",
